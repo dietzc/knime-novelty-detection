2031937d969e882532caba9ad7376d83ec988dd6
Adrian Nembach <adrian.nembach@gmx.net>
2015-06-25 17:29:23 +0200

    Implementation KNFST Novelty Scorer Node
    WIP

--- src/org/knime/knip/noveltydetection/nodes/knfstnoveltyscorer/KNFSTNoveltyScorerNodeModel.java
+++ src/org/knime/knip/noveltydetection/nodes/knfstnoveltyscorer/KNFSTNoveltyScorerNodeModel.java
@@ -50,8 +50,6 @@ package org.knime.knip.noveltydetection.nodes.knfstnoveltyscorer;
 
 import java.io.File;
 
-import net.imagej.ImgPlus;
-import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
 
 import org.knime.core.data.DataCell;
@@ -60,6 +58,7 @@ import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.DoubleCell;
 import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.BufferedDataTableHolder;
@@ -69,11 +68,11 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
-import org.knime.core.node.defaultnodesettings.SettingsModelString;
-import org.knime.knip.base.data.img.ImgPlusCell;
-import org.knime.knip.base.data.img.ImgPlusCellFactory;
-import org.knime.knip.base.data.img.ImgPlusValue;
-import org.knime.knip.qualityControl.patching.Patcher;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.knip.noveltydetection.knfst.alternative.KNFST;
+import org.knime.knip.noveltydetection.nodes.knfstlearner.KNFSTPortObject;
 
 /**
  * Crop BitMasks or parts of images according to a Labeling
@@ -95,17 +94,10 @@ public class KNFSTNoveltyScorerNodeModel<L extends Comparable<L>, T extends Real
          *
          * @return SettingsModel to store img column
          */
-        static SettingsModelString createImgColumnSelectionModel() {
-                return new SettingsModelString("ADRIAN", "");
-        }
 
-        static SettingsModelString createNumPatchesSelectionModel() {
-                return new SettingsModelString("NumPatches", "0");
-        }
+        DataTableSpec m_inTableSpec;
 
         /* SettingsModels */
-        private SettingsModelString m_imgColumnNameModel = createImgColumnSelectionModel();
-        private SettingsModelString m_numPatchesModel = createNumPatchesSelectionModel();
 
         /* Resulting BufferedDataTable */
         private BufferedDataTable m_data;
@@ -114,24 +106,24 @@ public class KNFSTNoveltyScorerNodeModel<L extends Comparable<L>, T extends Real
          * Constructor SegementCropperNodeModel
          */
         public KNFSTNoveltyScorerNodeModel() {
-                super(1, 1);
+                super(new PortType[] {KNFSTPortObject.TYPE, BufferedDataTable.TYPE}, new PortType[] {BufferedDataTable.TYPE});
         }
 
         /**
          * {@inheritDoc}
          */
         @Override
-        protected DataTableSpec[] configure(final DataTableSpec[] inSpecs) throws InvalidSettingsException {
+        protected DataTableSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
                 // TODO check inspec for img value column
 
-                return createOutSpec();
+                m_inTableSpec = (DataTableSpec) inSpecs[1];
+                return createOutSpec(m_inTableSpec);
         }
 
-        private DataTableSpec[] createOutSpec() {
-                DataColumnSpec sourceImgSpec = new DataColumnSpecCreator("Source Image", ImgPlusCell.TYPE).createSpec();
-                DataColumnSpec patchSpec = new DataColumnSpecCreator("Patch", ImgPlusCell.TYPE).createSpec();
+        private DataTableSpec[] createOutSpec(final DataTableSpec inTableSpec) {
+                DataColumnSpec scoreSpec = new DataColumnSpecCreator("Novelty Score", DoubleCell.TYPE).createSpec();
 
-                return new DataTableSpec[] {new DataTableSpec(sourceImgSpec, patchSpec)};
+                return new DataTableSpec[] {new DataTableSpec(inTableSpec, new DataTableSpec(scoreSpec))};
         }
 
         /**
@@ -139,43 +131,22 @@ public class KNFSTNoveltyScorerNodeModel<L extends Comparable<L>, T extends Real
          */
         @Override
         @SuppressWarnings({"unchecked"})
-        protected BufferedDataTable[] execute(final BufferedDataTable[] inData, final ExecutionContext exec) throws Exception {
+        protected BufferedDataTable[] execute(final PortObject[] inData, final ExecutionContext exec) throws Exception {
 
-                final BufferedDataContainer container = exec.createDataContainer(createOutSpec()[0]);
-                final ImgPlusCellFactory imgCellFac = new ImgPlusCellFactory(exec);
+                final BufferedDataContainer container = exec.createDataContainer(createOutSpec(m_inTableSpec)[0]);
 
-                int imgCellIdx = inData[0].getSpec().findColumnIndex(m_imgColumnNameModel.getStringValue());
-                int numPatches = Integer.parseInt(m_numPatchesModel.getStringValue());
+                final KNFST knfst = ((KNFSTPortObject) inData[0]).getKNFST();
+                final BufferedDataTable testData = (BufferedDataTable) inData[1];
 
-                if (imgCellIdx == -1) {
-                        throw new IllegalArgumentException("No Image Column found with name: " + m_imgColumnNameModel.getStringValue());
-                }
+                double[] scores = knfst.scoreTestData(testData);
+                int iterator = 0;
 
-                for (final DataRow row : inData[0]) {
-                        final ImgPlusValue<T> imgPlusValue = (ImgPlusValue<T>) row.getCell(imgCellIdx);
-                        DataCell[] cells = new DataCell[2];
-
-                        // get Img from ImgPlus
-                        Img<T> img = imgPlusValue.getImgPlus().getImg();
-
-                        // calculate parameters for patching
-                        long[] dimensionSizes = new long[img.numDimensions()];
-                        img.dimensions(dimensionSizes);
-                        int[] dimensions = new int[img.numDimensions()];
-                        for (int i = 0; i < img.numDimensions(); i++)
-                                dimensions[i] = i;
-                        int[] patchesPerDimension = Patcher.calculatePatchesPerDimension(numPatches, dimensionSizes);
-
-                        Object[] patches = Patcher.patchImg(img, dimensions, patchesPerDimension);
-
-                        for (int p = 0; p < patches.length; p++) {
-                                // TODO: write patch into ImgPlus
-                                ImgPlus<T> impPatch = new ImgPlus<T>((Img<T>) patches[p]);
-                                cells[0] = imgCellFac.createCell(imgPlusValue.getImgPlus());
-                                cells[1] = imgCellFac.createCell(impPatch);
-                                container.addRowToTable(new DefaultRow(row.getKey().toString() + "#" + p, cells));
+                for (DataRow row : testData) {
+                        DataCell[] cells = new DataCell[row.getNumCells() + 1];
+                        for (int c = 0; c < cells.length; c++) {
+                                cells[c] = (c < cells.length - 1) ? row.getCell(c) : new DoubleCell(scores[iterator++]);
                         }
-
+                        container.addRowToTable(new DefaultRow(row.getKey(), cells));
                 }
 
                 container.close();
@@ -204,8 +175,7 @@ public class KNFSTNoveltyScorerNodeModel<L extends Comparable<L>, T extends Real
          */
         @Override
         protected void loadValidatedSettingsFrom(final NodeSettingsRO settings) throws InvalidSettingsException {
-                m_imgColumnNameModel.loadSettingsFrom(settings);
-                m_numPatchesModel.loadSettingsFrom(settings);
+
         }
 
         /**
@@ -229,8 +199,7 @@ public class KNFSTNoveltyScorerNodeModel<L extends Comparable<L>, T extends Real
          */
         @Override
         protected void saveSettingsTo(final NodeSettingsWO settings) {
-                m_imgColumnNameModel.saveSettingsTo(settings);
-                m_numPatchesModel.saveSettingsTo(settings);
+
         }
 
         /**
@@ -246,7 +215,6 @@ public class KNFSTNoveltyScorerNodeModel<L extends Comparable<L>, T extends Real
          */
         @Override
         protected void validateSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
-                m_imgColumnNameModel.validateSettings(settings);
-                m_numPatchesModel.validateSettings(settings);
+
         }
 }
