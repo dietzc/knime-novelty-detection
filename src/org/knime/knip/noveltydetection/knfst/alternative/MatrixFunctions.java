6b17dc39953cde2a14ae2c6188c9a3f7855b8e9f
Adrian Nembach <adrian.nembach@gmx.net>
2015-06-25 10:48:28 +0200

    Addition of methods
    sqrt, concatHorizontally, concatVertically, pow

--- src/org/knime/knip/noveltydetection/knfst/alternative/MatrixFunctions.java
+++ src/org/knime/knip/noveltydetection/knfst/alternative/MatrixFunctions.java
@@ -35,23 +35,21 @@ public class MatrixFunctions {
                 return ones.scalarAdd(1);
         }
 
-        public static RealMatrix nullspace(RealMatrix matrix) {
-                SingularValueDecomposition svd = new SingularValueDecomposition(matrix);
-                int rank = svd.getRank();
-                RealMatrix V = svd.getV();
-                double[][] basisData = new double[V.getRowDimension()][V.getColumnDimension()];
-                V.copySubMatrix(0, V.getRowDimension() - 1, rank, V.getColumnDimension() - 1, basisData);
-                return MatrixUtils.createRealMatrix(basisData);
+        public static RealMatrix nullspace(final RealMatrix matrix) {
+                final SingularValueDecomposition svd = new SingularValueDecomposition(matrix);
+                final int rank = svd.getRank();
+                final RealMatrix V = svd.getV();
+                return V.getSubMatrix(0, V.getRowDimension() - 1, rank, V.getColumnDimension() - 1);
         }
 
-        public static double[] abs(double[] array) {
-                double[] abs = new double[array.length];
+        public static double[] abs(final double[] array) {
+                final double[] abs = new double[array.length];
                 for (int i = 0; i < array.length; i++)
                         abs[i] = Math.abs(array[i]);
                 return abs;
         }
 
-        public static int argmin(double[] array) {
+        public static int argmin(final double[] array) {
                 int index = 0;
                 for (int i = 0; i < array.length; i++) {
                         if (array[i] < array[index]) {
@@ -61,8 +59,8 @@ public class MatrixFunctions {
                 return index;
         }
 
-        public static RealMatrix rowMins(RealMatrix matrix) {
-                RealMatrix rowMins = MatrixUtils.createRealMatrix(matrix.getRowDimension(), 1);
+        public static RealMatrix rowMins(final RealMatrix matrix) {
+                final RealMatrix rowMins = MatrixUtils.createRealMatrix(matrix.getRowDimension(), 1);
 
                 for (int r = 0; r < matrix.getRowDimension(); r++) {
                         rowMins.setEntry(r, 0, matrix.getRowVector(r).getMinValue());
@@ -71,13 +69,92 @@ public class MatrixFunctions {
                 return rowMins;
         }
 
-        public static RealMatrix sqrt(RealMatrix matrix) {
-                double[][] data = matrix.getData();
+        public static RealMatrix sqrt(final RealMatrix matrix) {
+                final double[][] data = matrix.getData();
                 for (int r = 0; r < matrix.getRowDimension(); r++) {
                         for (int c = 0; c < matrix.getColumnDimension(); c++) {
                                 data[r][c] = Math.sqrt(data[r][c]);
                         }
                 }
                 return MatrixUtils.createRealMatrix(data);
+        }
+
+        public static RealMatrix multiplyElementWise(final RealMatrix matrix1, final RealMatrix matrix2) {
+                if (matrix1.getRowDimension() != matrix2.getRowDimension() || matrix1.getColumnDimension() != matrix2.getColumnDimension()) {
+                        throw new IllegalArgumentException("The matrices must be of the same dimensions!");
+                }
+
+                final RealMatrix result = matrix1.createMatrix(matrix1.getRowDimension(), matrix1.getColumnDimension());
+
+                for (int r = 0; r < matrix1.getRowDimension(); r++) {
+                        for (int c = 0; c < matrix1.getColumnDimension(); c++)
+                                result.setEntry(r, c, matrix1.getEntry(r, c) * matrix2.getEntry(r, c));
+                }
+
+                return result;
+        }
+
+        public static RealVector rowSums(final RealMatrix matrix) {
+                final RealVector rowSums = MatrixUtils.createRealVector(new double[matrix.getRowDimension()]);
+                for (int r = 0; r < matrix.getRowDimension(); r++) {
+                        final double[] row = matrix.getRow(r);
+                        for (double cell : row)
+                                rowSums.addToEntry(r, cell);
+                }
+                return rowSums;
+        }
+
+        public static RealVector sqrt(final RealVector vector) {
+                final RealVector result = vector.copy();
+                for (int e = 0; e < result.getDimension(); e++)
+                        result.setEntry(e, Math.sqrt(result.getEntry(e)));
+                return result;
+        }
+
+        public static RealMatrix pow(final RealMatrix matrix, final double power) {
+                final RealMatrix result = matrix.createMatrix(matrix.getRowDimension(), matrix.getColumnDimension());
+                for (int r = 0; r < result.getRowDimension(); r++) {
+                        for (int c = 0; c < result.getColumnDimension(); c++)
+                                result.setEntry(r, c, Math.pow(matrix.getEntry(r, c), power));
+                }
+                return result;
+        }
+
+        public static RealMatrix concatHorizontally(final RealMatrix left, final RealMatrix right) {
+                if (left.getRowDimension() != right.getRowDimension()) {
+                        throw new IllegalArgumentException("The matrices must have the same row dimension!");
+                }
+
+                final double[][] result = new double[left.getRowDimension()][left.getColumnDimension() + right.getColumnDimension()];
+
+                final int lc = left.getColumnDimension();
+
+                for (int r = 0; r < left.getRowDimension(); r++) {
+                        for (int c = 0; c < left.getColumnDimension(); c++)
+                                result[r][c] = left.getEntry(r, c);
+                        for (int c = 0; c < right.getColumnDimension(); c++)
+                                result[r][lc + c] = right.getEntry(r, c);
+                }
+
+                return MatrixUtils.createRealMatrix(result);
+        }
+
+        public static RealMatrix concatVertically(final RealMatrix top, final RealMatrix bottom) {
+                if (top.getColumnDimension() != bottom.getColumnDimension()) {
+                        throw new IllegalArgumentException("The matrices must have the same column dimension!");
+                }
+
+                final double[][] result = new double[top.getRowDimension() + bottom.getRowDimension()][top.getColumnDimension()];
+
+                final int tr = top.getRowDimension();
+
+                for (int c = 0; c < top.getColumnDimension(); c++) {
+                        for (int r = 0; r < top.getRowDimension(); r++)
+                                result[r][c] = top.getEntry(r, c);
+                        for (int r = 0; r < bottom.getRowDimension(); r++)
+                                result[tr + r][c] = bottom.getEntry(r, c);
+                }
+
+                return MatrixUtils.createRealMatrix(result);
         }
 }
