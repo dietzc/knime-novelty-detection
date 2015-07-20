cceb8b5e4a90163b54c8897e3e1f728e390ceb77
Adrian Nembach <adrian.nembach@gmx.net>
2015-07-20 16:18:28 +0200

    Tests of Apache Commons Math svd

---
+++ src/org/knime/knip/noveltydetection/knfst/alternative/ApacheCommonsMath.java
@@ -0,0 +1,18 @@
+package org.knime.knip.noveltydetection.knfst.alternative;
+
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
+
+public class ApacheCommonsMath {
+
+        public static void main(String[] args) {
+                double[][] elements = { {1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+                RealMatrix A = MatrixUtils.createRealMatrix(elements);
+
+                SingularValueDecomposition svd = new SingularValueDecomposition(A);
+                Test.printMatrix(svd.getS());
+                Test.printMatrix(svd.getV());
+                System.out.println(svd.getRank());
+        }
+}
