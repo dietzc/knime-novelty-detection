0acc1dfad71ff9520beb3478347250fb67376a11
Adrian Nembach <adrian.nembach@gmx.net>
2015-07-22 09:30:00 +0200

    Timing for testing

--- src/org/knime/knip/noveltydetection/knfst/alternative/MultiClassKNFST.java
+++ src/org/knime/knip/noveltydetection/knfst/alternative/MultiClassKNFST.java
@@ -57,21 +57,28 @@ public class MultiClassKNFST extends KNFST {
         @Override
         public double[] scoreTestData(double[][] test) {
                 // calculate nxm kernel matrix containing similarities between n training samples and m test samples
+                long time = System.currentTimeMillis();
                 RealMatrix kernelMatrix = m_kernel.kernelize(test);
+                System.out.println("Kernelizing: " + (System.currentTimeMillis() - time));
 
                 return score(kernelMatrix);
         }
 
         private double[] score(final RealMatrix kernelMatrix) {
                 // projected test samples:
+                long time1 = System.currentTimeMillis();
                 final RealMatrix projectionVectors = kernelMatrix.transpose().multiply(m_projection);
 
                 //squared euclidean distances to target points:
+                long time2 = System.currentTimeMillis();
+                System.out.println("Projecting test samples: " + (time2 - time1));
                 final RealMatrix squared_distances = squared_euclidean_distances(projectionVectors, m_targetPoints);
 
                 // novelty scores as minimum distance to one of the target points
+                long time3 = System.currentTimeMillis();
+                System.out.println("Calculating squared euclidean distances: " + (time3 - time2));
                 final RealVector scoreVector = MatrixFunctions.sqrt(MatrixFunctions.rowMins(squared_distances));
-
+                System.out.println("Calculating score vector: " + (System.currentTimeMillis() - time3));
                 return scoreVector.toArray();
         }
 
