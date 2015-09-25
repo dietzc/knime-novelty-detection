36d6931e80f07ee3e97fb63a88c394f8c8abc311
Adrian Nembach <adrian.nembach@gmx.net>
2015-09-25 19:01:34 +0200

    Add option to sort table before processing

--- src/org/knime/knip/noveltydetection/nodes/knfstlearner/KNFSTLearnerNodeDialog.java
+++ src/org/knime/knip/noveltydetection/nodes/knfstlearner/KNFSTLearnerNodeDialog.java
@@ -3,6 +3,7 @@ package org.knime.knip.noveltydetection.nodes.knfstlearner;
 import org.knime.core.data.DoubleValue;
 import org.knime.core.data.StringValue;
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
 import org.knime.core.node.defaultnodesettings.DialogComponentColumnFilter;
 import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
@@ -22,5 +23,8 @@ public class KNFSTLearnerNodeDialog<L extends Comparable<L>> extends DefaultNode
 
                 addDialogComponent(new DialogComponentStringSelection(KNFSTLearnerNodeModel.createKernelFunctionSelectionModel(), "Kernel",
                                 KNFSTLearnerNodeModel.AVAILABLE_KERNELS));
+
+                addDialogComponent(new DialogComponentBoolean(KNFSTLearnerNodeModel.createSortTableModel(),
+                                "Sort table (select only if table is not already sorted by class"));
         }
 }
